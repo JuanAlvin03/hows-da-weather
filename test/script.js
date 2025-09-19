d807c5f710b8e99b3330782f362b6ddcb5cbc761
JuanAlvin03 <juanalvinss03@gmail.com>
2025-09-19 22:01:40 +1000

    playing round with the data
    try stuff with the data

--- test/script.js
+++ test/script.js
@@ -1,3 +1,4 @@
+/*
 const getWeatherData = async () => {
     const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m");
     const data = await response.json();
@@ -5,3 +6,4 @@ const getWeatherData = async () => {
 };
 
 getWeatherData();
+*/
